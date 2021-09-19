ad77a25a379424a6118c4b7b0d15673c2c859bb4
Daniel Guecha <guechadaniel@gmail.com>
2021-09-19 01:15:54 -0500

    feature/initial configure eviroment done ✨

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express, { json } from 'express';
+import morgan from 'morgan';
+import pkg from '../package.json';
+import helmet from 'helmet';
+
+//importing routes
+// import helloRoutes from './routes/hello.routes';
+
+const app = express();
+
+// Cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+// settings
+app.set('pkg', pkg);
+
+// middleware
+app.use(morgan('dev'));
+app.use(json());
+app.use(helmet());
+
+const os = require("os");
+const hostname = os.hostname();
+
+// welcome routes
+app.get('/', (req, res) => {
+  res.json({
+      author: app.get('pkg').author,
+      name: app.get('pkg').name,
+      description: app.get('pkg').description,
+      version: app.get('pkg').version, 
+      hostname: hostname
+  })
+})
+
+// routes
+// app.use('/api/hello', helloRoutes);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const hello = 'hello world';
+import app from './app';
+import '@babel/polyfill';
+// import config from './config';
+// import 'regenerator-runtime';
+
+async function main() {
+    await app.listen(3000, '0.0.0.0');
+    console.log('Server on port 3000')
+};
+
+main();
